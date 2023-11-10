257905ef4d3171ac8c25c3bb99ac7b0fe7dd9dcc
scarletvargas <scarletvf47@gmail.com>
2023-11-10 09:20:44 -0400

    Adding assets/icons files

---
+++ src/assets/icons/framer.js
@@ -0,0 +1,19 @@
+export const framer = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-framer"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      stroke-width={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M6 15h12l-12 -12h12v6h-12v6l6 6v-6"></path>
+    </svg>
+  );
+};
---
+++ src/assets/icons/nextjs.js
@@ -0,0 +1,20 @@
+export const nextjs = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-nextjs"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      stroke-width={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M9 15v-6l7.745 10.65a9 9 0 1 1 2.255 -1.993"></path>
+      <path d="M15 12v-3"></path>
+    </svg>
+  );
+};
