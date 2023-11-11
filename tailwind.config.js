1cbba51f2dc7f365fc20ac72c12f29b065bd4ae3
scarletvargas <scarletvf47@gmail.com>
2023-11-11 11:54:07 -0400

    configuring tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
       colors: {
         primary: "#281AEF",
         secondary: "#07042F",
@@ -17,17 +27,17 @@ module.exports = {
         softblue200: "#9D9ACB",
       },
       screens: {
-        'md': '810px',
-        'xl': '1200px',
+        md: "810px",
+        xl: "1200px",
       },
       boxShadow: {
-        '3xl': '0 4px 30px 4px rgba(101, 101, 114, 0.2)',
+        "3xl": "0 4px 30px 4px rgba(101, 101, 114, 0.2)",
       },
       borderRadius: {
-        '4xl': '22px',
-        '5xl': '26px',
+        "4xl": "22px",
+        "5xl": "26px",
       },
     },
   },
   plugins: [],
-}
+};
