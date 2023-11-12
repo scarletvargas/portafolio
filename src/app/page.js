db6e60cfa55ea618d74f0b3504898383dc88dec0
scarletvargas <scarletvf47@gmail.com>
2023-11-12 18:39:00 -0400

    updating page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/Components/HomePage/Hero";
+import MarqueeHero from "@/Components/HomePage/MarqueeHero";
 import SectionProjects from "@/Components/HomePage/SectionProjects";
 
 function Home() {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <MarqueeHero />
       <SectionProjects />
     </div>
   );
