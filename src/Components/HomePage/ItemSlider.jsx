2875e56c129b1ed6a51933c57190cc0b8ac4cd54
scarletvargas <scarletvf47@gmail.com>
2023-11-12 18:38:35 -0400

    Finished slider

--- src/Components/HomePage/ItemSlider.jsx
+++ src/Components/HomePage/ItemSlider.jsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
-import ProjectOne from "@/assets/prueba.webp";
 import ButtonPrimary from "../ButtonPrimary";
 
-function ItemSlider({ isFirst, isLast }) {
+function ItemSlider({ index, isFirst, isLast, handleHover, isClasses, imgProject, nameProject, description }) {
   const imageClass = isFirst ? "rounded-l-5xl" : isLast ? "rounded-r-5xl" : "";
   return (
-    <div className="relative flex flex-1 hover:flex-2 group w-[740px] xl:w-[1130px] h-[500px] duration-150 ease-in ">
+    <div
+      className={`relative flex ${
+        isClasses ? "flex-2" : "flex-1"
+      } group w-[740px] xl:w-[1130px] h-[500px] duration-150 ease-in`}
+      onMouseEnter={() => handleHover(index)}
+    >
       <div className="h-[500px] flex-col justify-center items-center">
         <Image
-          src={ProjectOne}
+          src={imgProject}
           placeholder="blur"
-          className={`grayscale h-full group-hover:grayscale-0 object-center object-cover ${imageClass}`}
+          className={`h-full ${
+            isClasses ? "grayscale-0" : "grayscale"
+          } object-center object-cover ${imageClass}`}
         />
       </div>
-      <div className="z-20 absolute h-full w-full hidden group-hover:flex flex-col items-center justify-center gap-[30px]">
+      <div className="z-20 absolute h-full w-full hidden group-hover:flex flex-col items-center justify-center gap-[30px] animate-fadeInY">
         <p className="py-3 px-[22px] bg-white text-secondary text-[22px]/7 font-bold rounded-xl shadow-3xl">
-          nombre del proyecto
+          {nameProject}
         </p>
         <p className="py-2 px-3 bg-white text-secondary text-sm leading-[18px] font-normal rounded-xl shadow-3xl">
-          Lorem ipsum dolor sit amet.
+          {description}
         </p>
         <ButtonPrimary text="Explore More" />
       </div>
--- src/Components/HomePage/Slider.jsx
+++ src/Components/HomePage/Slider.jsx
@@ -1,11 +1,21 @@
+'use client'
+import React, { useState } from "react";
 import ItemSlider from "./ItemSlider";
+import ProjectTest from "@/assets/prueba.webp";
+
 
 function Slider() {
+  const [hoveredItem, setHoveredItem] = useState(0);
+
+  const handleHover = (index) => {
+    setHoveredItem(index);
+  };
+
   return (
-    <div className="z-10 relative hidden md:flex flex-row w-[740px] xl:w-[1130px] h-[500px]">
-      <ItemSlider isFirst />
-      <ItemSlider />
-      <ItemSlider isLast />
+    <div className="hidden md:flex flex-row w-[740px] xl:w-[1130px] h-[500px]">
+      <ItemSlider index={0} isFirst handleHover={handleHover} isClasses={hoveredItem === 0} imgProject={ProjectTest} nameProject="Name Project" description="This is a brief description"/>
+      <ItemSlider index={1} handleHover={handleHover} isClasses={hoveredItem === 1} imgProject={ProjectTest} nameProject="Name Project" description="This is a brief description"/>
+      <ItemSlider index={2} isLast handleHover={handleHover} isClasses={hoveredItem === 2} imgProject={ProjectTest} nameProject="Name Project" description="This is a brief description"/>
     </div>
   );
 }
