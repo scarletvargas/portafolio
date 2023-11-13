3fc6e52a640a84788b24d8b3ec3786839374b266
scarletvargas <scarletvf47@gmail.com>
2023-11-13 15:08:39 -0400

    Adding new colors and dark theme option to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
@@ -18,13 +19,17 @@ module.exports = {
         },
       },
       colors: {
-        primary: "#281AEF",
-        secondary: "#07042F",
         gray100: "#EBEBEB",
         gray200: "#848297",
         gray300: "#656572",
+        gray400: "#353252",
+        navy200: "#040217",
+        primary: "#281AEF",
+        secondary: "#07042F",
+        sky: "#85DEFF",
         softblue100: "#DEDBFE",
         softblue200: "#9D9ACB",
+        white100: "#F4F4F6",
       },
       flex: {
         2: "2 1 70%",
