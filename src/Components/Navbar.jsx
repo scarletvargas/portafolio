c97f960a14b2181bb95c595d52e93901b9deacbe
scarletvargas <scarletvf47@gmail.com>
2023-11-13 15:02:33 -0400

    Adding the dark theme to the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,53 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import ButtonSecond from "./ButtonSecond";
+import ItemNav from "./ItemNav";
+import { Sun } from "@/Icons/Sun";
+import { Moon } from "@/Icons/Moon";
 
 function Navbar() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <nav className="flex justify-between items-center p-7 text-secondary">
-      <Link href="/">
-        <p className="text-xl/5 font-extrabold">DevScar</p>
-      </Link>
-      <ul className="hidden sm:flex sm:flex-row sm:justify-between sm:items-center sm:gap-5 md:gap-7 font-medium text-base/4">
-        <li>
-          <Link className="hover:text-primary" href="/">Home</Link>
-        </li>
-        <li>
-          <Link className="hover:text-primary" href="/#skills">Skills</Link>
-        </li>
-        <li>
-          <Link className="hover:text-primary" href="/#projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <ButtonSecond text="Contact" />
-          </Link>
-        </li>
-      </ul>
-      <button className="block sm:hidden">hola</button>
-    </nav>
+    <header className="flex justify-center items-center dark:bg-navy200">
+      <nav className="flex justify-between items-center p-7 w-80 md:w-[810px] xl:w-[1200px]">
+        <Link href="/">
+          <p className="text-xl/5 font-extrabold text-secondary dark:text-white">
+            DevScar
+          </p>
+        </Link>
+        <ul className="hidden sm:flex sm:flex-row sm:justify-between sm:items-center sm:gap-5 md:gap-7 font-medium text-base/4">
+          <ItemNav nameItem="Home" href="/" />
+          <ItemNav nameItem="Skills" href="/#skills" />
+          <ItemNav nameItem="Projects" href="/#projects" />
+          <button onClick={handleChangeTheme}>
+            {theme === "light" ? <Moon /> : <Sun />}
+          </button>
+          <li>
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/scarletvargas-systems-engineer/"
+            >
+              <ButtonSecond text="Contact" />
+            </Link>
+          </li>
+        </ul>
+        <button className="block sm:hidden">hola</button>
+      </nav>
+    </header>
   );
 }
 
---
+++ src/Icons/Sun.js
@@ -0,0 +1,20 @@
+export const Sun = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brightness-2 stroke-white hover:stroke-primary"
+      width={28}
+      height={28}
+      viewBox="0 0 24 24"
+      stroke-width={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M6 6h3.5l2.5 -2.5l2.5 2.5h3.5v3.5l2.5 2.5l-2.5 2.5v3.5h-3.5l-2.5 2.5l-2.5 -2.5h-3.5v-3.5l-2.5 -2.5l2.5 -2.5z"></path>
+    </svg>
+  );
+};
