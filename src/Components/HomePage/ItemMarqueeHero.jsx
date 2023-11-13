6705fcd23439f7e383acef3c2aea5ec0b18df947
scarletvargas <scarletvf47@gmail.com>
2023-11-13 15:13:06 -0400

    Adding the classes of the new colors

--- src/Components/HomePage/ItemMarqueeHero.jsx
+++ src/Components/HomePage/ItemMarqueeHero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ItemMarqueeHero({ text, iconSvg: Icon }) {
   return (
-    <div className="flex flex-row justify-start items-center px-5 py-2 gap-2 w-auto bg-[#F4F4F6] rounded-lg">
+    <div className="flex flex-row justify-start items-center px-5 py-2 gap-2 w-auto bg-white100 rounded-lg">
       <Icon />
       <p className="text-secondary text-xl">{text}</p>
     </div>
