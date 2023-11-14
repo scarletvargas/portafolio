8dcf0a956f8777bf83861ae09a6f478539832663
scarletvargas <scarletvf47@gmail.com>
2023-11-14 14:01:12 -0400

    Adding icons for the navbar

---
+++ src/Icons/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Menu = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-secondary hover:stroke-primary dark:stroke-white dark:hover:stroke-primary"
+      width={28}
+      height={28}
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M4 6l16 0"></path>
+      <path d="M4 12l16 0"></path>
+      <path d="M4 18l16 0"></path>
+    </svg>
+  );
+};
