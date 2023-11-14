1f913dd9288cd920933d841cf35de7d7ce0009dd
scarletvargas <scarletvf47@gmail.com>
2023-11-14 14:00:43 -0400

    Footer finished

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,51 +1,55 @@
 import ButtonPrimary from "./ButtonPrimary";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
-library.add(faLinkedin);
+import { Linkedin } from "@/Icons/Linkedin";
+import { Github } from "@/Icons/Github";
+import { Instagram } from "@/Icons/Instagram";
 
 function Footer() {
   return (
-    <footer className="bg-[#040217] text-white flex flex-col items-center w-auto pt-[70px]">
-      <div className="flex flex-col items-center justify-center pb-24 gap-7">
-        <h3 className="text-center max-w-[300px] md:max-w-[500px] xl:max-w-[600px] font-bold text-[30px] md:text-[50px] leading-[38px] md:leading-[70px]">
-          Ready to <span className="text-[#85DEFF]">Build Awesome</span>{" "}
-          Project?
+    <footer className="bg-navy200 dark:bg-softblue200 text-white dark:text-secondary flex flex-col items-center w-auto pt-[70px]">
+      <div className="max-w-[350px] md:max-w-[600px] flex flex-col items-center justify-center pb-24 gap-7">
+        <h3 className="text-center font-bold text-[30px] md:text-[50px] leading-[38px] md:leading-[70px]">
+          Let's build an{" "}
+          <span className="text-sky dark:text-primary">Amazing Project</span>{" "}
+          Together
         </h3>
-        <p className="max-w-[450px] text-center text-gray200 text-base/7">
-          Memulai sebuah projek dengan riset dan memvalidasi ideas dengan
-          usability-testing agar maksimal.
+        <p className="text-center text-gray200 dark:text-gray400 text-base/7">
+          Along with the newest technologies on the market to create attractive
+          user interfaces! Do you have questions, proposals or just want to
+          chat? I'm just a click away. Let's make ideas come true together!
         </p>
-        <ButtonPrimary text="Hire Me Now" />
+        <ButtonPrimary text="Contact Me" />
         {/* <marquee></marquee> */}
       </div>
-      <hr className="bg-[#353252] w-[330px] md:w-[700px] xl:w-[1130px] h-[1px] border-0" />
+      <hr className="bg-gray400 dark:bg-gray100 w-[330px] md:w-[700px] xl:w-[1130px] h-[1px] border-0" />
       <div className="flex flex-col xl:flex-row justify-center items-start pt-24 gap-12 w-[330px] md:w-[700px] xl:w-[1130px]">
         <div className="flex flex-col gap-7 xl:w-1/3">
-          <h2 className="text-white font-bold text-xl/5">DevScar</h2>
-          <p className="text-lg/7 max-w-[300px] md:max-w-[350px] xl:w-[300px]">
-            Minimalist Framer template for any freelancer in the world.
+          <h2 className="text-white dark:text-secondary tracking-wide font-bold text-xl/5">
+            DevScarlet
+          </h2>
+          <p className="text-lg/7 max-w-[300px] font-medium md:max-w-[350px] xl:w-[300px]">
+            Find out more about me as a frontend developer.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-between gap-[60px] w-full">
           <div className="flex flex-col gap-5 w-1/2 xl:pl-56">
             <p className="text-lg leading-[18px] font-semibold">Useful Links</p>
             <Link
-              className="text-base leading-[18px] text-gray200 hover:text-white"
-              href="/portfolios"
+              className="text-base leading-[18px] font-medium hover:font-semibold text-gray200 dark:text-gray400 hover:text-white dark:hover:text-secondary"
+              href="/#projects"
             >
-              Portfolios
+              Projects
             </Link>
             <Link
-              className="text-base leading-[18px] text-gray200 hover:text-white"
+              className="text-base leading-[18px] font-medium hover:font-semibold text-gray200 dark:text-gray400 hover:text-white dark:hover:text-secondary"
               href="/#skills"
             >
               My Skills
             </Link>
             <Link
-              className="text-base leading-[18px] text-gray200 hover:text-white"
-              href="/contact"
+              className="text-base leading-[18px] font-medium hover:font-semibold text-gray200 dark:text-gray400 hover:text-white dark:hover:text-secondary"
+              href="https://www.linkedin.com/in/scarletvargas-systems-engineer/"
+              target="_blank"
             >
               Contact
             </Link>
@@ -55,50 +59,41 @@ function Footer() {
             <Link
               target="_blank"
               href="https://www.linkedin.com/in/scarletvargas-systems-engineer/"
-              className="flex flex-row items-center gap-2.5"
+              className="flex flex-row items-center gap-2.5 group"
             >
-              <FontAwesomeIcon
-                icon="fa-brands fa-linkedin"
-                size="2xs"
-                style={{ color: "#ffffff" }}
-                className="w-6 h-6"
-              />
-              <p>Scarlet Vargas</p>
-            </Link>
-            <Link
-              target="_blank"
-              href="https://www.linkedin.com/in/scarletvargas-systems-engineer/"
-              className="flex flex-row items-center gap-2.5"
-            >
-              <FontAwesomeIcon
-                icon="fa-brands fa-linkedin"
-                size="2xs"
-                style={{ color: "#ffffff" }}
-                className="w-6 h-6"
-              />
-              <p className="text-base leading-[18px] text-white">
+              <Linkedin className="stroke-white dark:stroke-secondary group-hover:stroke-primary" />
+              <p className="group-hover:text-primary text-base leading-[18px] font-medium">
                 Scarlet Vargas
               </p>
             </Link>
             <Link
               target="_blank"
-              href="https://www.linkedin.com/in/scarletvargas-systems-engineer/"
-              className="flex flex-row items-center gap-2.5"
+              href="https://github.com/scarletvargas"
+              className="flex flex-row items-center gap-2.5 group"
             >
-              <FontAwesomeIcon
-                icon="fa-brands fa-linkedin"
-                size="2xs"
-                style={{ color: "#ffffff" }}
-                className="w-6 h-6"
-              />
-              <p>Scarlet Vargas</p>
+              <Github className="stroke-white dark:stroke-secondary group-hover:stroke-primary" />
+              <p className="group-hover:text-primary text-base leading-[18px] font-medium">
+                scarletvargas
+              </p>
+            </Link>
+            <Link
+              target="_blank"
+              href="https://www.instagram.com/scarlet.vf/"
+              className="flex flex-row items-center gap-2.5 group"
+            >
+              <Instagram className="stroke-white dark:stroke-secondary group-hover:stroke-primary" />
+              <p className="group-hover:text-primary text-base leading-[18px] font-medium">
+                @scarlet.vf
+              </p>
             </Link>
           </div>
         </div>
       </div>
       <div className="flex justify-center py-12">
-        <p className="text-gray200 max-w-[300px] sm:max-w-none text-center text-sm/6 font-normal">
-          All Rights Reserved by Portoku by BuildWithAngga 2024
+        <p className="text-gray200 dark:text-gray400 max-w-[300px] sm:max-w-none text-center text-sm/6 font-medium">
+          Copyright © 2023{" "}
+          <span className="font-semibold tracking-wide ">DevScarlet</span>. All
+          rights reserved.
         </p>
       </div>
     </footer>
