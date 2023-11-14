01ddf9ce1c1d9f0a88a8ccb435b6e4418485a54e
scarletvargas <scarletvf47@gmail.com>
2023-11-13 23:33:13 -0400

    Adding changes and dark mode to CardSkills

--- src/Components/HomePage/CardSkill.jsx
+++ src/Components/HomePage/CardSkill.jsx
@@ -1,13 +1,18 @@
 function CardSkill({ title, description, iconSvg: Icon, width }) {
   return (
-    <div className={`group flex flex-col items-start gap-6 p-10 bg-white100 hover:bg-secondary rounded-5xl w-[330px] ${width}`}>
-      <div className="flex items-center justify-center w-[65px] h-[65px] bg-softblue100 group-hover:bg-primary rounded-4xl">
-        <Icon />
+    <div
+      className={`group flex flex-col items-start gap-6 p-10 bg-white100 dark:bg-secondary hover:bg-secondary dark:hover:bg-white100 rounded-5xl w-[330px] ${width} h-auto md:h-[400px] xl:h-[335px]`}
+    >
+      <div className="flex items-center justify-center w-[65px] h-[65px] bg-softblue100 dark:bg-primary group-hover:bg-primary dark:group-hover:bg-softblue100 rounded-4xl">
+        <Icon
+          strokeWidth="1.5"
+          className="stroke-primary dark:stroke-white group-hover:stroke-white dark:group-hover:stroke-primary"
+        />
       </div>
-      <h4 className="text-secondary group-hover:text-white text-xl leading-5 lg:max-w-md font-bold">
+      <h4 className="text-secondary dark:text-white group-hover:text-white dark:group-hover:text-secondary text-xl/6 lg:max-w-md font-bold">
         {title}
       </h4>
-      <p className="text-gray300 group-hover:text-white text-base leading-7 lg:max-w-md font-normal">
+      <p className="text-gray300 dark:text-white group-hover:text-white dark:group-hover:text-gray300 text-base/7 lg:max-w-md font-normal">
         {description}
       </p>
     </div>
