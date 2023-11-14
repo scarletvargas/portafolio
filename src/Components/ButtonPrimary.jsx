030544494a9b8b6fc645fdf400e1fd011813d97e
scarletvargas <scarletvf47@gmail.com>
2023-11-14 13:53:57 -0400

    Modifying the buttons

--- src/Components/ButtonPrimary.jsx
+++ src/Components/ButtonPrimary.jsx
@@ -1,7 +1,12 @@
-function ButtonPrimary({onClick, text}) {
+function ButtonPrimary({ onClick, text, additionalClasses }) {
   return (
-    <button onClick={onClick} className="py-4 px-7 rounded-full cursor-pointer bg-primary dark:bg-secondary hover:bg-secondary dark:hover:bg-primary font-semibold text-white text-base/4">{text}</button>
-  )
+    <button
+      onClick={onClick}
+      className={`py-4 px-7 rounded-full cursor-pointer bg-primary dark:bg-secondary hover:bg-secondary dark:hover:bg-primary font-semibold text-white text-base/4 ${additionalClasses}`}
+    >
+      {text}
+    </button>
+  );
 }
 
-export default ButtonPrimary
+export default ButtonPrimary;
--- src/Components/ButtonSecond.jsx
+++ src/Components/ButtonSecond.jsx
@@ -1,8 +1,8 @@
-function ButtonSecond({ onClick, text }) {
+function ButtonSecond({ onClick, text, additionalClasses }) {
   return (
     <button
       onClick={onClick}
-      className="py-4 px-7 rounded-full cursor-pointer bg-secondary dark:bg-primary hover:bg-primary dark:hover:bg-secondary font-semibold text-white text-base/4"
+      className={`py-4 px-7 rounded-full cursor-pointer bg-secondary dark:bg-primary hover:bg-primary dark:hover:bg-secondary font-semibold text-white text-base/4 ${additionalClasses}`}
     >
       {text}
     </button>
