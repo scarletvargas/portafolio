4e0f4b061a36e5a1737c9cbd979c462498c14562
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:33:26 -0400

    Code formatted with new prettier settings

--- src/Icons/code.js
+++ src/Icons/code.js
@@ -1,21 +1,21 @@
-export const Code = ({className, strokeWidth}) => {
+export const Code = ({ className, strokeWidth }) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns='http://www.w3.org/2000/svg'
       className={className}
       width={30}
       height={30}
-      viewBox="0 0 24 24"
+      viewBox='0 0 24 24'
       strokeWidth={strokeWidth}
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M7 8l-4 4l4 4"></path>
-      <path d="M17 8l4 4l-4 4"></path>
-      <path d="M14 4l-4 16"></path>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M7 8l-4 4l4 4'></path>
+      <path d='M17 8l4 4l-4 4'></path>
+      <path d='M14 4l-4 16'></path>
     </svg>
   );
 };
