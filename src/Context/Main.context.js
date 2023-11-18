771cc9aa62fb2c08b88cb393d65595ad9bc40dc7
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:37:20 -0400

    Adding MainContext

---
+++ src/Context/Main.context.js
@@ -0,0 +1,31 @@
+'use client';
+import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
+
+const MainContext = createContext();
+
+export const MainProvider = ({ children }) => {
+  const themes = useMemo(() => ['light', 'dark'], []) ;
+
+  const [theme, setTheme] = useState(themes[0]);
+
+  useEffect(() => {
+    themes.map((iterationTheme) => {
+      document.documentElement.classList.remove(iterationTheme);
+    });
+
+    document.documentElement.classList.add(theme);
+
+    let localTheme = localStorage.getItem('theme') || theme;
+    setTheme(localTheme);
+  }, [theme, themes]);
+
+  return (
+    <MainContext.Provider value={{ themes, theme, setTheme }}>{children}</MainContext.Provider>
+  );
+};
+
+MainContext.displayName = 'MainContext';
+
+export const useMainContext = () => {
+  return useContext(MainContext);
+};
