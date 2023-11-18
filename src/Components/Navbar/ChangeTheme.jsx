5e822b7075172f8701eaffd5930c305b1ecfaad9
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:37:53 -0400

    Modifying ChangeTheme

--- src/Components/Navbar/ChangeTheme.jsx
+++ src/Components/Navbar/ChangeTheme.jsx
@@ -1,60 +1,26 @@
-"use client";
-import { useEffect, useState } from "react";
-import { Moon } from "@/Icons/Moon";
-import { Sun } from "@/Icons/Sun";
+'use client';
+import { Moon } from '@/Icons/Moon';
+import { Sun } from '@/Icons/Sun';
+import { useMainContext } from '@/Context/Main.context';
 
 function ChangeTheme({ className }) {
-  // Initialize the theme state based on localStorage or system preference
-  const [theme, setTheme] = useState(() => {
-    // Check if the system prefers a dark color scheme
-    const systemPrefersDark =
-      typeof window !== "undefined" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    // Check if localStorage is available
-    const localStorageTheme =
-      typeof window !== "undefined" && localStorage.getItem("theme");
-
-    // Set the initial theme based on localStorage or system preference
-    if (
-      localStorageTheme === "dark" ||
-      (!localStorageTheme && systemPrefersDark)
-    ) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  });
-
-  // Effect to update the HTML element class and localStorage when theme changes
-  useEffect(() => {
-    // Check if the window object is defined
-    if (typeof window !== "undefined") {
-      const htmlElement = document.querySelector("html");
-
-      // Apply or remove the 'dark' class based on the selected theme
-      if (theme === "dark") {
-        htmlElement.classList.add("dark");
-      } else {
-        htmlElement.classList.remove("dark");
-      }
-
-      // Update the localStorage when theme changes
-      localStorage.setItem("theme", theme);
-    }
-  }, [theme]);
+  const { themes, theme, setTheme } = useMainContext();
 
   // Function to handle theme change on button click
   const handleChangeTheme = () => {
-    // Toggle between 'light' and 'dark' themes
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
+    let currentIndex = themes.indexOf(theme);
+    let nextIndex = currentIndex == 0 ? 1 : 0;
+    const nextTheme = themes[nextIndex];
+
+    localStorage.setItem('theme', nextTheme);
+
+    setTheme(nextTheme);
   };
 
   // Render a button with an icon representing the current theme
   return (
     <button className={className} onClick={handleChangeTheme}>
-      {theme === "light" ? <Moon /> : <Sun />}
+      {theme === 'light' ? <Moon /> : <Sun />}
     </button>
   );
 }
