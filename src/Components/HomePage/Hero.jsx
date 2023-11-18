eb5e1d1a8c7afc590ca72bc38b80091a5c6b0c5c
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:32:18 -0400

    Content changes

--- src/Components/HomePage/Hero.jsx
+++ src/Components/HomePage/Hero.jsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <div className="py-[30px] md:py-[50px] xl:py-[70px] flex justify-center">
       <div className="flex flex-col justify-center items-center gap-[30px] px-6 py-[30px] md:px-0 md:pt-0 md:pb-5 max-w-sm md:max-w-3xl xl:max-w-7xl">
-        <p className="text-secondary dark:text-white text-xl leading-5 font-medium">Hola! I am Scarlet,</p>
+        <p className="text-secondary dark:text-white text-xl leading-5 font-medium">Hi! I am Scarlet,</p>
         <h1 className="text-secondary dark:text-white text-center font-bold text-5xl leading-[3rem] md:text-8xl md:leading-[6rem] xl:text-[160px] xl:leading-[9rem]">
           Frontend Developer
         </h1>
--- src/Components/HomePage/SectionSkills.jsx
+++ src/Components/HomePage/SectionSkills.jsx
@@ -1,46 +1,44 @@
-import CardSkill from "./CardSkill";
-import { Code } from "@/Icons/Code";
-import { Tools } from "@/Icons/Tools";
-import { Versions } from "@/Icons/Versions";
-import { Rocket } from "@/Icons/Rocket";
+import CardSkill from './CardSkill';
+import { Code } from '@/Icons/Code';
+import { Tools } from '@/Icons/Tools';
+import { Versions } from '@/Icons/Versions';
+import { Rocket } from '@/Icons/Rocket';
 
 function SectionSkills() {
   return (
-    <section id="skills" className="flex flex-col items-center justify-center">
-      <div className="w-[320px] md:w-[720px] xl:w-[1130px] flex flex-col items-start justify-center gap-2.5 py-[30px] md:pt-[50px] xl:pt-[70px]">
-        <div className="flex flex-col gap-5 items-start justify-center">
-          <p className="font-bold text-sm leading-[14px] text-primary dark:text-sky">
-            ABOUT ME?
-          </p>
-          <h3 className="max-w-[350px] md:max-w-sm font-bold text-secondary dark:text-white text-[28px] leading-[38px] md:text-3xl/10 xl:text-[36px] xl:leading-[46px]">
+    <section id='skills' className='flex flex-col items-center justify-center'>
+      <div className='w-[320px] md:w-[720px] xl:w-[1130px] flex flex-col items-start justify-center gap-2.5 py-[30px] md:pt-[50px] xl:pt-[70px]'>
+        <div className='flex flex-col gap-5 items-start justify-center'>
+          <p className='font-bold text-sm leading-[14px] text-primary dark:text-sky'>ABOUT ME?</p>
+          <h3 className='max-w-[350px] md:max-w-sm font-bold text-secondary dark:text-white text-[28px] leading-[38px] md:text-3xl/10 xl:text-[36px] xl:leading-[46px]'>
             Frontend Developer Are My Skills
           </h3>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center md:flex-row flex-wrap gap-[30px] w-[320px] md:w-[720px] xl:w-[1130px] ">
+      <div className='flex flex-col items-center justify-center md:flex-row flex-wrap gap-[30px] w-[320px] md:w-[720px] xl:w-[1130px] '>
         <CardSkill
-          title="Responsive User Interfaces"
-          description="Ability to transform designs into code, ensuring that interfaces are visually attractive and functional on various devices and screens."
+          title='Responsive User Interfaces'
+          description='Ability to transform designs into code, ensuring that interfaces are visually appealing and functional on various devices and screens.'
           iconSvg={Code}
-          width="md:w-[400px] xl:w-[570px]"
+          width='md:w-[400px] xl:w-[570px]'
         />
         <CardSkill
-          title="Adaptability and Continuous Learning"
-          description="Willingness and ability to address challenges, solve problems creatively and confront new technologies."
+          title='Adaptability and Continuous Learning'
+          description='Willingness and ability to address challenges, creatively solve problems, and embrace new technologies.'
           iconSvg={Rocket}
-          width="md:w-[290px] xl:w-[400px]"
+          width='md:w-[290px] xl:w-[400px]'
         />
         <CardSkill
-          title="Version and Repository Management"
-          description="Use version control systems, like Git, and manage repositories to track and collaborate on code effectively."
+          title='Version and Repository Management'
+          description='Proficient in using version control systems, such as Git, and managing repositories to effectively track and collaborate on code.'
           iconSvg={Versions}
-          width="md:w-[290px] xl:w-[400px]"
+          width='md:w-[290px] xl:w-[400px]'
         />
         <CardSkill
-          title="Frontend Frameworks and Libraries"
-          description="Use of frontend frameworks and libraries, such as React, Next.js, Redux, Formik... to facilitate the creation of complex web applications."
+          title='Frontend Frameworks and Libraries'
+          description='Utilization of frontend frameworks and libraries, such as React, Next.js, Redux, TailwindCss, Formik, React-Hook-Form, to streamline the development of complex web applications.'
           iconSvg={Tools}
-          width="md:w-[400px] xl:w-[570px]"
+          width='md:w-[400px] xl:w-[570px]'
         />
       </div>
     </section>
