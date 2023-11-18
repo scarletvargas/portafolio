ca1ad5a49906ca0e1cbfc4b945f8f948d94b49b6
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:36:58 -0400

    Adding Main Provider, modifying metadata and formatting the code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,32 @@
-import { Poppins } from "next/font/google";
-import Footer from "@/Components/Footer";
-import Navbar from "@/Components/Navbar/Navbar";
-import ScrollToTopButton from "@/Components/ScrollToTopButton";
-import "./globals.css";
+import { Poppins } from 'next/font/google';
+import Footer from '@/Components/Footer';
+import Navbar from '@/Components/Navbar/Navbar';
+import ScrollToTopButton from '@/Components/ScrollToTopButton';
+import { MainProvider } from '@/Context/Main.context';
+import './globals.css';
 
 const poppins = Poppins({
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal"],
-  subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "DevScarlet - Frontend Developer",
-  description: "Explora el mundo del desarrollo web con DevScarlet, un apasionado Frontend Developer. Descubre proyectos innovadores y experiencias web cautivadoras creadas con React, Next.js, Tailwind CSS y una variedad de frameworks y librerías. ¡Bienvenido a un viaje donde la estética se encuentra con la funcionalidad!",
+  title: 'DevScarlet - Frontend Developer',
+  description:
+    'Explore the world of web development with DevScarlet, a passionate Frontend Developer. Discover innovative projects and captivating web experiences created with React, Next.js, Tailwind CSS, and a variety of frameworks and libraries. Welcome to a journey where aesthetics meets functionality!',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={poppins.className}>
-        <Navbar />
-        {children}
-        <ScrollToTopButton />
-        <Footer />
+        <MainProvider>
+          <Navbar />
+          {children}
+          <ScrollToTopButton />
+          <Footer />
+        </MainProvider>
       </body>
     </html>
   );
