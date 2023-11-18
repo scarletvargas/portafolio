4603997ee8d3a5ab0f3f58a744d5b8fbd0045509
scarletvargas <scarletvf47@gmail.com>
2023-11-17 21:34:00 -0400

    Code formatted with new prettier settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import Carousel from "@/Components/HomePage/Carousel";
-import Hero from "@/Components/HomePage/Hero";
-import MarqueeHero from "@/Components/HomePage/MarqueeHero";
-import SectionProjects from "@/Components/HomePage/SectionProjects";
-import SectionSkills from "@/Components/HomePage/SectionSkills";
+import Carousel from '@/Components/HomePage/Carousel';
+import Hero from '@/Components/HomePage/Hero';
+import MarqueeHero from '@/Components/HomePage/MarqueeHero';
+import SectionProjects from '@/Components/HomePage/SectionProjects';
+import SectionSkills from '@/Components/HomePage/SectionSkills';
 
 function Home() {
   return (
-    <div className="overflow-hidden dark:bg-navy200">
+    <div className='overflow-hidden dark:bg-navy200'>
       <Hero />
       <MarqueeHero />
       <SectionSkills />
