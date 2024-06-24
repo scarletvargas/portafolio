2e90439b4829df3d81a534ce692ccc1f0a0d2a52
scarletvargas <scarletvf47@gmail.com>
2024-06-24 18:15:38 -0400

    adding translations to not found page

--- src/app/[locale]/404/page.tsx
+++ src/app/[locale]/404/page.tsx
@@ -1,23 +1,26 @@
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 import { ButtonPrimary } from '@/Components';
 
-export default function NotFound() {
+export default function NotFoundPage() {
+  const t = useTranslations('NotFoundPage');
+
   return (
     <div className='flex flex-col items-start justify-center gap-7 py-32 px-14 xl:px-28 dark:bg-navy200'>
       <p className='font-bold text-sm leading-[14px] text-primary dark:text-sky'>404</p>
-      <h3 className='max-w-[350px] md:max-w-sm font-bold text-secondary dark:text-white text-[28px] leading-[38px] md:text-3xl/10 xl:text-[36px] xl:leading-[46px]'>
-        Page Not Found
+      <h3 className='max-w-[350px] font-bold text-secondary dark:text-white text-[28px] leading-[38px] md:text-3xl/10 xl:text-[36px] xl:leading-[46px]'>
+        {t('title')}
       </h3>
       <div className='flex flex-col md:flex-row md:items-center gap-7'>
         <Link
           href='/'
           className='text-gray400 dark:text-gray200 hover:text-primary dark:hover:text-sky'
         >
-          Go back to home page
+          {t('goBack')}
         </Link>
         <Link target='_blank' href='https://www.linkedin.com/in/scarletvargas-systems-engineer/'>
-          <ButtonPrimary>Contact Me</ButtonPrimary>
+          <ButtonPrimary>{t('contactMe')}</ButtonPrimary>
         </Link>
       </div>
     </div>
