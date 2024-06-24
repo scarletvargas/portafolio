0175021656b10cbdc8cb4ce5e23baa0c3cc4e4e7
scarletvargas <scarletvf47@gmail.com>
2024-06-23 21:17:10 -0400

    fix error

--- src/Components/HomePage/MarqueeHero.tsx
+++ src/Components/HomePage/MarqueeHero.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { ItemMarqueeHero } from '@/Components/HomePage';
 import * as Icons from '@/Icons';
@@ -51,17 +51,17 @@ export const MarqueeHero: React.FC = () => {
     setScrollStartX(marqueeRef.current!.scrollLeft);
   };
 
-  const handleMouseMove = (event: MouseEvent) => {
-    if (!isDragging) return;
-    const deltaX = event.clientX - dragStartX;
-    marqueeRef.current!.scrollLeft = scrollStartX - deltaX;
-  };
-
-  const handleMouseUp = () => {
+  const handleMouseUp = useCallback(() => {
     setIsDragging(false);
-  };
+  }, []);
 
   useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!isDragging) return;
+      const deltaX = event.clientX - dragStartX;
+      marqueeRef.current!.scrollLeft = scrollStartX - deltaX;
+    };
+
     if (isDragging) {
       document.addEventListener('mousemove', handleMouseMove);
       document.addEventListener('mouseup', handleMouseUp);
@@ -73,7 +73,7 @@ export const MarqueeHero: React.FC = () => {
       document.removeEventListener('mousemove', handleMouseMove);
       document.removeEventListener('mouseup', handleMouseUp);
     };
-  }, [isDragging]);
+  }, [isDragging, dragStartX, scrollStartX, handleMouseUp]);
 
   return (
     <div className='mx-auto max-w-[900px]'>
