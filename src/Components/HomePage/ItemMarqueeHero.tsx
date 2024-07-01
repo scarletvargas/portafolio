09d410d730d324359fb2e20895c44c317e184567
scarletvargas <scarletvf47@gmail.com>
2024-06-30 23:11:06 -0400

    optimizing the component

--- src/Components/HomePage/ItemMarqueeHero.tsx
+++ src/Components/HomePage/ItemMarqueeHero.tsx
@@ -1,13 +1,20 @@
+import { GetIcon } from '../GetIcon';
+
 interface ItemMarqueeHeroProps {
-  title: string;
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  iconName: string;
 }
 
-export const ItemMarqueeHero: React.FC<ItemMarqueeHeroProps> = ({ title, icon: Icon }) => {
+export const ItemMarqueeHero: React.FC<ItemMarqueeHeroProps> = ({ iconName }) => {
   return (
     <div className='select-none flex flex-row justify-start items-center px-5 py-2 gap-2 w-auto bg-white100 rounded-lg'>
-      <Icon className='stroke-secondary' width={38} height={38} strokeWidth='1.5' />
-      <p className='text-secondary text-xl'>{title}</p>
+      <GetIcon
+        icon={iconName}
+        className='stroke-secondary'
+        width={38}
+        height={38}
+        strokeWidth='1.5'
+      />
+      <p className='text-secondary text-xl'>{iconName}</p>
     </div>
   );
 };
